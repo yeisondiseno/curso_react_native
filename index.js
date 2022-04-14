035613ac33833fff8911ab1e68a10a47df78e5c5
Yeison Montoya <yeisondiseno@gmail.com>
2022-04-14 11:05:22 -0500

    Y.Montoya: save

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function index() {
+  return (
+    <View>
+          <Text>
+        adadawdawd
+              qeawdawdaw
+        awdawd
+              </Text>
+    </View>
+  )
+}
